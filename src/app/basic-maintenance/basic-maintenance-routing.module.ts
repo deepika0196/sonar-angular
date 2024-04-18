e7bd8b3dc8b3a6b889b605f0908613413eb24e1c
BALI Maninder <maninder.bali@soprasteria.com>
2024-04-18 19:39:38 +0530

    testing the table component

--- src/app/basic-maintenance/basic-maintenance-routing.module.ts
+++ src/app/basic-maintenance/basic-maintenance-routing.module.ts
@@ -3,23 +3,38 @@ import { RouterModule, Routes } from '@angular/router';
 import { BasicMaintenanceComponent } from './basic-maintenance.component';
 import { CampoDeActuacionComponent } from './components/campo-de-actuacion/campo-de-actuacion.component';
 import { environment } from 'src/environments/environment';
+import { EntidadesSolicitantesComponent } from 'src/app/basic-maintenance/components/entidades-solicitantes/entidades-solicitantes.component';
 
-const routes: Routes = [{ path: '', component: BasicMaintenanceComponent },
-{
-  path: 'campoDeActuacion',
-  component: CampoDeActuacionComponent,
-  // canActivate: [ArqRoleGuard],
-  // canActivateChild: [ArqRoleGuard],
-  data: {
-    host: environment.settings.host,
-    nombreApp: environment.gvlogin.aplicacion,
-    gvloginUrl: environment.gvlogin.url,
-    enable: environment.gvlogin.enable,
+const routes: Routes = [
+  { path: '', component: BasicMaintenanceComponent },
+  {
+    path: 'campoDeActuacion',
+    component: CampoDeActuacionComponent,
+    // canActivate: [ArqRoleGuard],
+    // canActivateChild: [ArqRoleGuard],
+    data: {
+      host: environment.settings.host,
+      nombreApp: environment.gvlogin.aplicacion,
+      gvloginUrl: environment.gvlogin.url,
+      enable: environment.gvlogin.enable,
+    },
   },
-}];
+  {
+    path: 'entidadesSolicitantes',
+    component: EntidadesSolicitantesComponent,
+    // canActivate: [ArqRoleGuard],
+    // canActivateChild: [ArqRoleGuard],
+    data: {
+      host: environment.settings.host,
+      nombreApp: environment.gvlogin.aplicacion,
+      gvloginUrl: environment.gvlogin.url,
+      enable: environment.gvlogin.enable,
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class BasicMaintenanceRoutingModule { }
+export class BasicMaintenanceRoutingModule {}
