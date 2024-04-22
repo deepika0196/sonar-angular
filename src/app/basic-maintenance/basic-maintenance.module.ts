8800e0ee40ec7768716104d0cfa948c32fcdeaa0
ppooja <pooja.pooja@soprasteria.com>
2024-04-22 14:43:34 +0530

    add, update, delete and alert dialog

--- src/app/basic-maintenance/basic-maintenance.module.ts
+++ src/app/basic-maintenance/basic-maintenance.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { BasicMaintenanceRoutingModule } from './basic-maintenance-routing.module';
-import { BasicMaintenanceComponent } from './basic-maintenance.component';
-import { RepresentantesDeEntidadComponent } from './components/representantes-de-entidad/representantes-de-entidad.component';
-import { RequerimientosSubsanacionComponent } from './components/requerimientos-subsanacion/requerimientos-subsanacion.component';
-import { CampoDeActuacionComponent } from './components/campo-de-actuacion/campo-de-actuacion.component';
-import { EntidadesSolicitantesComponent } from './components/entidades-solicitantes/entidades-solicitantes.component';
-import { PrimeNgModule } from '../prime-ng.module';
-import { AlertDialogComponent } from './components/campo-de-actuacion/alert-dialog/alert-dialog.component';
-import { TranslocoRootModule } from '../transloco-root.module';
+import { BasicMaintenanceRoutingModule } from "./basic-maintenance-routing.module";
+import { BasicMaintenanceComponent } from "./basic-maintenance.component";
+import { RepresentantesDeEntidadComponent } from "./components/representantes-de-entidad/representantes-de-entidad.component";
+import { RequerimientosSubsanacionComponent } from "./components/requerimientos-subsanacion/requerimientos-subsanacion.component";
+import { CampoDeActuacionComponent } from "./components/campo-de-actuacion/campo-de-actuacion.component";
+import { EntidadesSolicitantesComponent } from "./components/entidades-solicitantes/entidades-solicitantes.component";
+import { PrimeNgModule } from "../prime-ng.module";
+import { AlertDialogComponent } from "./components/campo-de-actuacion/alert-dialog/alert-dialog.component";
+import { TranslocoRootModule } from "../transloco-root.module";
 
 @NgModule({
   declarations: [
@@ -18,13 +18,13 @@ import { TranslocoRootModule } from '../transloco-root.module';
     RequerimientosSubsanacionComponent,
     CampoDeActuacionComponent,
     EntidadesSolicitantesComponent,
-    AlertDialogComponent
+    AlertDialogComponent,
   ],
   imports: [
     CommonModule,
     BasicMaintenanceRoutingModule,
     PrimeNgModule,
-    TranslocoRootModule
-  ]
+    TranslocoRootModule,
+  ],
 })
-export class BasicMaintenanceModule { }
+export class BasicMaintenanceModule {}
