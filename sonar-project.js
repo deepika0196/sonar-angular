d4fcdacbc4663b5081619b02264e48515eaac8ba
BALI Maninder <maninder.bali@soprasteria.com>
2024-04-29 16:01:21 +0530

    added sonar server

---
+++ sonar-project.js
@@ -0,0 +1,18 @@
+var sonarqubeScanner = require('sonarqube-scanner');
+sonarqubeScanner(
+  {
+    serverUrl: 'http://10.6.76.231:9000',
+    token: '2d68d9ab11709b0d2c2656309d487a11ff02e616',
+    options: {
+      'sonar.projectName': 'ECMCA FRONTEND',
+      'sonar.sources': '.',
+      'sonar.inclusions': 'src/**', // Entry point of your code
+      'sonar.exclusions': 'src/assets/**, **/*.spec.ts, src/environments/**', // Entry point of your code which you want to exclude
+      // "sonar.exclusions": "src/assets/**,**/*.spec.ts,encryption.service.ts", // Entry point of your code which you want to exclude
+      'sonar.language': 'ts',
+      // 'sonar.testExecutionReportPaths': 'reports/ut_report.xml', // Entry point of test report xml file 'sonar.'sonar.
+      // 'typescript.lcov.reportPaths': 'coverage/{project-name}/lcov.info', // Entry point of coverage report lcov file also added project 'coverage/project-name/lcov.info
+    },
+  }
+  // () => process.exit()
+);
