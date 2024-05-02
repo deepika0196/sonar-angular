a05e096c3f90a8f4ad700506934a006e1819c17b
BALI Maninder <maninder.bali@soprasteria.com>
2024-05-02 13:30:03 +0530

    pushing changes for karma missing plugins

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -9,41 +6,42 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      // ! start sonar reports configuration
+      require('karma-phantomjs-launcher'),
+      require('karma-sonarqube-unit-reporter'),
+      // ! end sonar reports configuration
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+    // ! start sonar reports configuration
+    sonarQubeUnitReporter: {
+      sonarQubeVersion: 'LATEST',
+      outputFile: 'reports/ut_report.xml',
+      useBrowserName: false,
     },
-    coverageReporter: {
+    // ! end sonar reports configuration
+    coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/arquetipo-angular'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reports: ['html', 'lcovonly', 'text-summary'], // ! sonarqube can only read lcov files.
+      fixWebpackSourcePaths: true,
     },
-
-    browsers: ['PhantomJS', 'ChromeHeadless', 'Chrome'],
-    customLaunchers: {
-      ChromeHeadlessCI: { base: 'ChromeHeadless', flags: ['--no-sandbox'] },
-    },
-    
-    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    reporters: ['progress', 'kjhtml', 'sonarqubeUnit'], // ! Add sonarqubeUnit in array
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    // browsers: ["Chrome"],
+    // ! start sonar reports configuration
+    browsers: ['PhantomJS', 'ChromeHeadless', 'Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: { base: 'ChromeHeadless', flags: ['--no-sandbox'] },
+    },
+    // ! end sonar reports configuration
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
   });
 };
