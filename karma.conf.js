8b84fba430b2273b93b6eaed42538531a57d737d
SINGH Deepika <deepika.singh@soprasteria.com>
2024-05-02 06:57:24 +0000

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -32,12 +32,17 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
+
+    browsers: ['PhantomJS', 'ChromeHeadless', 'Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: { base: 'ChromeHeadless', flags: ['--no-sandbox'] },
+    },
+    
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
