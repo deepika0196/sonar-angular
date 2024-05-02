b06a63d7594d372f030607a60dee372180d815a8
BALI Maninder <maninder.bali@soprasteria.com>
2024-05-02 13:43:03 +0530

    pushing changes for karma missing plugins

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,6 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -11,10 +14,18 @@ module.exports = function (config) {
       // ! start sonar reports configuration
       require('karma-phantomjs-launcher'),
       require('karma-sonarqube-unit-reporter'),
-      // ! end sonar reports configuration
     ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
     },
     // ! start sonar reports configuration
     sonarQubeUnitReporter: {
@@ -22,26 +33,22 @@ module.exports = function (config) {
       outputFile: 'reports/ut_report.xml',
       useBrowserName: false,
     },
-    // ! end sonar reports configuration
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/arquetipo-angular'),
       reports: ['html', 'lcovonly', 'text-summary'], // ! sonarqube can only read lcov files.
       fixWebpackSourcePaths: true,
     },
-    port: 9876,
-    reporters: ['progress', 'kjhtml', 'sonarqubeUnit'], // ! Add sonarqubeUnit in array
+
+    reporters: ['progress', 'kjhtml', 'sonarqubeUnit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    // browsers: ["Chrome"],
-    // ! start sonar reports configuration
+    singleRun: false,
+    restartOnFileChange: true,
     browsers: ['PhantomJS', 'ChromeHeadless', 'Chrome'],
     customLaunchers: {
       ChromeHeadlessCI: { base: 'ChromeHeadless', flags: ['--no-sandbox'] },
     },
-    // ! end sonar reports configuration
-    singleRun: false,
-    restartOnFileChange: true,
   });
 };
