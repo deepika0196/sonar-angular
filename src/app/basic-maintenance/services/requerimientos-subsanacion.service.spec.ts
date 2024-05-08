db7b96a12e3147cdc7242ac4adf740a11182361c
ppooja <pooja.pooja@soprasteria.com>
2024-05-08 11:49:27 +0530

    test file updated

--- src/app/basic-maintenance/services/requerimientos-subsanacion.service.spec.ts
+++ src/app/basic-maintenance/services/requerimientos-subsanacion.service.spec.ts
@@ -1,15 +1,27 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { RequerimientosSubsanacionService } from './requerimientos-subsanacion.service';
 
 describe('RequerimientosSubsanacionService', () => {
   let service: RequerimientosSubsanacionService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [RequerimientosSubsanacionService],
+    });
     service = TestBed.inject(RequerimientosSubsanacionService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
+  afterEach(() => {
+    httpMock.verify();
+  });
+  
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
