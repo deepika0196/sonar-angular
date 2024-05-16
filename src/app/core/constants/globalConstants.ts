45f87ee9aa5bb111cc08993b69e88fb6ed757d01
Meenakshi265 <104715464+Meenakshi265@users.noreply.github.com>
2024-05-16 13:39:52 +0530

    resolve review comments

--- src/app/core/constants/globalConstants.ts
+++ src/app/core/constants/globalConstants.ts
@@ -2,7 +2,7 @@ export class GlobalConstant {
   //----------Text Data Type Constants----------\\
   public static ecmca = 'ECMCA';
   public static RegistroDeEntidades = 'Registro de entidades';
-  public static ddmmyy = 'dd/mm/yyyy';
+  public static ddmmyy = 'dd/mm/yy';
   public static ddmmyyyy = 'dd/MM/yyyy';
   public static ddmmyyyyWithHyphen = 'dd-MM-yyyy';
 }
