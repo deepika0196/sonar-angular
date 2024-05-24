12bd39ff17d014ca02ff69af88bafd618ff42903
BALI Maninder <maninder.bali@soprasteria.com>
2024-05-24 15:30:29 +0530

    wrote unit test for service and component

--- src/app/basic-maintenance/components/requerimientos-subsanacion/requerimientos-subsanacion.component.spec.ts
+++ src/app/basic-maintenance/components/requerimientos-subsanacion/requerimientos-subsanacion.component.spec.ts
@@ -1,25 +1,136 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
 import { RequerimientosSubsanacionComponent } from './requerimientos-subsanacion.component';
-import { TranslocoRootModule } from '@app/transloco-root.module';
+import { RequerimientosSubsanacionService } from '@app/basic-maintenance/services/requerimientos-subsanacion.service';
+import { of, throwError } from 'rxjs';
+import { RequerimientosSubsanacion } from '@app/basic-maintenance/interfaces/requerimientos-subsanacion';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('RequerimientosSubsanacionComponent', () => {
   let component: RequerimientosSubsanacionComponent;
   let fixture: ComponentFixture<RequerimientosSubsanacionComponent>;
+  let requerimientosSubsanacionService: jasmine.SpyObj<RequerimientosSubsanacionService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TranslocoRootModule, HttpClientModule],
-      declarations: [ RequerimientosSubsanacionComponent ]
-    })
-    .compileComponents();
+    const requerimientosSubsanacionServiceSpy = jasmine.createSpyObj(
+      'RequerimientosSubsanacionService',
+      ['getRequerimientosSubsanacions']
+    );
 
+    await TestBed.configureTestingModule({
+      declarations: [RequerimientosSubsanacionComponent],
+      providers: [
+        {
+          provide: RequerimientosSubsanacionService,
+          useValue: requerimientosSubsanacionServiceSpy,
+        },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    requerimientosSubsanacionService = TestBed.inject(
+      RequerimientosSubsanacionService
+    ) as jasmine.SpyObj<RequerimientosSubsanacionService>;
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(RequerimientosSubsanacionComponent);
     component = fixture.componentInstance;
+    const mockData: RequerimientosSubsanacion[] = [
+      { codigo: '1', derequerimiento: 'Desc', derequerimientoVal: 'Desc Val' },
+    ];
+    requerimientosSubsanacionService.getRequerimientosSubsanacions.and.returnValue(
+      of({ response: mockData, success: true })
+    );
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch all requerimientos subsanacion on init', () => {
+    const mockData: RequerimientosSubsanacion[] = [
+      { codigo: '1', derequerimiento: 'Desc', derequerimientoVal: 'Desc Val' },
+    ];
+    requerimientosSubsanacionService.getRequerimientosSubsanacions.and.returnValue(
+      of({ response: mockData, success: true })
+    );
+
+    component.ngOnInit();
+
+    expect(
+      requerimientosSubsanacionService.getRequerimientosSubsanacions
+    ).toHaveBeenCalled();
+    expect(component.requerimientosSubsanacions).toEqual(mockData);
+    expect(component.cloneRequerimientosSubsanacionRecords).toEqual(mockData);
+  });
+
+  // it('should handle error while fetching requerimientos subsanacion', () => {
+  //   requerimientosSubsanacionService.getRequerimientosSubsanacions.and.returnValue(
+  //     throwError(() => new Error('Error fetching data'))
+  //   );
+
+  //   component.ngOnInit();
+
+  //   expect(
+  //     requerimientosSubsanacionService.getRequerimientosSubsanacions
+  //   ).toHaveBeenCalled();
+  //   expect(component.requerimientosSubsanacions).toBeUndefined();
+  //   expect(component.cloneRequerimientosSubsanacionRecords).toBeUndefined();
+  // });
+
+  it('should clear all filters', () => {
+    component.codigo = '1';
+    component.derequerimiento = 'Desc';
+    component.derequerimientoVal = 'Desc Val';
+    component.cloneRequerimientosSubsanacionRecords = [
+      { codigo: '1', derequerimiento: 'Desc', derequerimientoVal: 'Desc Val' },
+    ];
+
+    component.clearAll();
+
+    expect(component.codigo).toBe('');
+    expect(component.derequerimiento).toBe('');
+    expect(component.derequerimientoVal).toBe('');
+    expect(component.requerimientosSubsanacions).toEqual(
+      component.cloneRequerimientosSubsanacionRecords
+    );
+  });
+
+  it('should filter records based on input values', () => {
+    component.cloneRequerimientosSubsanacionRecords = [
+      { codigo: '1', derequerimiento: 'Desc1', derequerimientoVal: 'DescVal1' },
+      { codigo: '2', derequerimiento: 'Desc2', derequerimientoVal: 'DescVal2' },
+    ];
+
+    component.codigo = '1';
+    component.derequerimiento = 'Desc1';
+    component.derequerimientoVal = 'DescVal1';
+
+    component.filterHandler();
+
+    expect(component.requerimientosSubsanacions.length).toBe(1);
+    expect(component.requerimientosSubsanacions[0].codigo).toBe('1');
+  });
+
+  it('should handle no filters applied', () => {
+    component.cloneRequerimientosSubsanacionRecords = [
+      { codigo: '1', derequerimiento: 'Desc1', derequerimientoVal: 'DescVal1' },
+      { codigo: '2', derequerimiento: 'Desc2', derequerimientoVal: 'DescVal2' },
+    ];
+
+    component.codigo = '';
+    component.derequerimiento = '';
+    component.derequerimientoVal = '';
+
+    component.filterHandler();
+
+    expect(component.requerimientosSubsanacions.length).toBe(2);
+  });
+
+  afterEach(() => {
+    component.ngOnDestroy();
+    expect(component.requerimientosSubsanacions).toEqual([]);
+    expect(component.cloneRequerimientosSubsanacionRecords).toEqual([]);
   });
 });
--- src/app/basic-maintenance/services/campo-de-actuacion.service.spec.ts
+++ src/app/basic-maintenance/services/campo-de-actuacion.service.spec.ts
@@ -1,28 +1,150 @@
 import { TestBed } from '@angular/core/testing';
+import { CampoDeActuacionService } from './campo-de-actuacion.service';
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
-import { CampoDeActuacionService } from './campo-de-actuacion.service';
+import { CustomResponse } from '@app/shared/services/common.service';
+import { CampoDeActuacion } from '../interfaces/campoDeActuacion';
+import { UrlEndpoints } from '@app/core/contsants/urlEndpoint';
 
 describe('CampoDeActuacionService', () => {
   let service: CampoDeActuacionService;
-  let httpMock: HttpTestingController;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [CampoDeActuacionService],
     });
+
     service = TestBed.inject(CampoDeActuacionService);
-    httpMock = TestBed.inject(HttpTestingController);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   afterEach(() => {
-    httpMock.verify();
+    httpTestingController.verify();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('getCampoDeActuacions', () => {
+    it('should call get with the correct URL', () => {
+      const mockResponse: CustomResponse<CampoDeActuacion> = {
+        response: [
+          {
+            codigo: '001',
+            deseccion: 'Test 1',
+            deseccionVal: 'Test Val 1',
+          },
+        ],
+        success: true,
+      };
+
+      service.getCampoDeActuacions().subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+
+      const req = httpTestingController.expectOne(
+        service.urlBuilder(UrlEndpoints.reccaSecciones)
+      );
+      expect(req.request.method).toEqual('GET');
+      req.flush(mockResponse);
+    });
+  });
+
+  describe('postCampoDeActuacions', () => {
+    it('should call post with the correct URL and data', () => {
+      const mockRequest: CampoDeActuacion = {
+        codigo: '001',
+        deseccion: 'Test 1',
+        deseccionVal: 'Test Val 1',
+      };
+
+      const mockResponse: CustomResponse<CampoDeActuacion> = {
+        response: [
+          {
+            codigo: '001',
+            deseccion: 'Test 1',
+            deseccionVal: 'Test Val 1',
+          },
+        ],
+        success: true,
+      };
+
+      service.postCampoDeActuacions(mockRequest).subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+
+      const req = httpTestingController.expectOne(
+        service.urlBuilder(UrlEndpoints.reccaSecciones)
+      );
+      expect(req.request.method).toEqual('POST');
+      expect(req.request.body).toEqual(mockRequest);
+      req.flush(mockResponse);
+    });
+  });
+
+  describe('updateCampoDeActuacions', () => {
+    it('should call put with the correct URL and data', () => {
+      const mockRequest: CampoDeActuacion = {
+        codigo: '001',
+        deseccion: 'Test 1',
+        deseccionVal: 'Test Val 1',
+      };
+
+      const mockResponse: CustomResponse<CampoDeActuacion> = {
+        response: [
+          {
+            codigo: '001',
+            deseccion: 'Test 1',
+            deseccionVal: 'Test Val 1',
+          },
+        ],
+        success: true,
+      };
+
+      service.updateCampoDeActuacions(mockRequest).subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+
+      const req = httpTestingController.expectOne(
+        service.urlBuilder(UrlEndpoints.reccaSecciones)
+      );
+      expect(req.request.method).toEqual('PUT');
+      expect(req.request.body).toEqual(mockRequest);
+      req.flush(mockResponse);
+    });
+  });
+
+  describe('deleteCampoDeActuacions', () => {
+    it('should call delete with the correct URL', () => {
+      const codigo = '123';
+      const mockResponse: CustomResponse<CampoDeActuacion> = {
+        response: [
+          {
+            codigo: '001',
+            deseccion: 'Test 1',
+            deseccionVal: 'Test Val 1',
+          },
+        ],
+        success: true,
+      };
+
+      service.deleteCampoDeActuacions(codigo).subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+
+      const req = httpTestingController.expectOne(
+        `${service.urlBuilder(UrlEndpoints.reccaSecciones)}/${codigo}`
+      );
+      expect(req.request.method).toEqual('DELETE');
+      req.flush(mockResponse);
+    });
+  });
+
+  describe('urlBuilder', () => {
+    it('should build the correct URL', () => {
+      const url = service.urlBuilder(UrlEndpoints.reccaSecciones);
+      expect(url).toEqual(`${service.apiUrl}${UrlEndpoints.reccaSecciones}`);
+    });
   });
 });
