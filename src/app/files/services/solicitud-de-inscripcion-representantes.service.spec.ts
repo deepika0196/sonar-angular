d766f22b624a8b8f98e38c0d227dc756b3fd622a
BALI Maninder <maninder.bali@soprasteria.com>
2024-05-28 00:44:40 +0530

    adding unit test for sonar coverage

--- src/app/files/services/solicitud-de-inscripcion-representantes.service.spec.ts
+++ src/app/files/services/solicitud-de-inscripcion-representantes.service.spec.ts
@@ -1,20 +1,76 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { SolicitudDeInscripcionRepresentantesService } from './solicitud-de-inscripcion-representantes.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UrlEndpoints } from '@app/core/contsants/urlEndpoint';
+import { RepresentantesLegal } from '@app/files/interfaces/solicitud-de-inscripcion';
+import { CustomResponse } from '@app/shared/services/common.service';
 
 describe('SolicitudDeInscripcionRepresentantesService', () => {
   let service: SolicitudDeInscripcionRepresentantesService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [SolicitudDeInscripcionRepresentantesService],
     });
+
     service = TestBed.inject(SolicitudDeInscripcionRepresentantesService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  describe('getByRepresentantesNifCif', () => {
+    it('should call get with the correct URL and parameter', () => {
+      const cifNif = '12345678A';
+      const mockResponse: CustomResponse<RepresentantesLegal> = {
+        response: [
+          {
+            apellidos: '',
+            codmun: '',
+            codpro: '',
+            cp: '',
+            domicilio: '',
+            email: '',
+            fax: '',
+            id: 0,
+            nifcif: '',
+            nombre: '',
+            telefono: '',
+          },
+        ],
+        success: true,
+      };
+
+      service.getByRepresentantesNifCif(cifNif).subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+
+      const req = httpTestingController.expectOne(
+        (req) =>
+          req.method === 'GET' &&
+          req.url ===
+            service.urlBuilder(UrlEndpoints.reccaRepresentantes) +
+              `/findByNifcif/${cifNif}`
+      );
+
+      expect(req.request.method).toEqual('GET');
+      req.flush(mockResponse);
+    });
+  });
+
+  describe('urlBuilder', () => {
+    it('should build the correct URL', () => {
+      const url = service.urlBuilder(UrlEndpoints.reccaRepresentantes);
+      expect(url).toEqual(
+        `${service.apiUrl}${UrlEndpoints.reccaRepresentantes}`
+      );
+    });
   });
 });
