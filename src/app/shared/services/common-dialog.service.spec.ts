eb071b278d44fcf91ea73c7582d287fa44125636
ppooja <pooja.pooja@soprasteria.com>
2024-05-28 15:28:52 +0530

    test case fixed

--- src/app/shared/services/common-dialog.service.spec.ts
+++ src/app/shared/services/common-dialog.service.spec.ts
@@ -1,13 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CommonDialogService } from './common-dialog.service';
+import { TranslocoRootModule } from '@app/transloco-root.module';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { DialogService } from 'primeng/dynamicdialog';
 
 describe('CommonDialogService', () => {
   let service: CommonDialogService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [TranslocoRootModule, HttpClientTestingModule],
+      providers: [CommonDialogService, DialogService],
+    });
     service = TestBed.inject(CommonDialogService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
